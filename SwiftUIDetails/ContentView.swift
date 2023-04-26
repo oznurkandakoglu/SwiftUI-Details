b2a9516c87aa3b2daf3929794c86e89037f4464e
Öznur Kandakoğlu <oznurkandakoglu@gmail.com>
2023-04-26 14:39:45 +0300

    Initial Commit

---
+++ SwiftUIDetails/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SwiftUIDetails
+//
+//  Created by Tiga  on 26.04.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIDetails/SwiftUIDetailsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDetailsApp.swift
+//  SwiftUIDetails
+//
+//  Created by Tiga  on 26.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDetailsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
